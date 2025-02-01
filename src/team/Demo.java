d9dd40d7bba56dd8d2f6e2ea954949c618c13093
salsh <salsh@Sally.lan>
2025-01-31 20:55:37 -0500

    writing a method

--- src/team/Demo.java
+++ src/team/Demo.java
@@ -1,5 +1,15 @@
 package team;
 
 public class Demo {
+	public static void main(String[] args) {
+		
+		int a = 1;
+		int b = 2;
+		 if (a > b) {
+			 System.out.println("a greater than b");
+		 }else {
+			 System.out.println("b greater than a");
+		 }
+	}
 
 }
