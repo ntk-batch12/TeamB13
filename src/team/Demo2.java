fbc80b8acbdea52fbd5f5547dde5447985562401
Shahed <Shahedalramdani@gmail.com>
2025-01-31 21:05:53 -0500

    Update Demo2.java

--- src/team/Demo2.java
+++ src/team/Demo2.java
@@ -3,118 +3,14 @@ package team;
 public class Demo2 {
 
   	public static void main(String[] args) {
-				/**
-			From Team 1
-			*/
-				/**
-			From Team 1
-			*/
-				/**
-			From Team 1
-			*/
-		
+	
 		
 		int bat = 3;
 		int ball = 2;
 		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-			System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);//pull practice team 1
-//pull practice team 1
-//pull practice team 1
-//pull practice team 1
-//pull practice team 1
-
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);	System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);	System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-
-			/**
-		more	From Team 1
-			*/
-
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);	System.out.println(bat + ball);
-		System.out.println(bat + ball);
-			/**
-		more	From Team 1
-			*/
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);	System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);	System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);	System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);	System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);	System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);	System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);	System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);	System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);	System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
-		System.out.println(bat + ball);
+	
+		
 	}
 	//what is this ?
-
+        //  We don't know!!
 }
