d22366cca3536b942d67445c5256072a3241917d
ntk-batch12 <ahmad.noor24@outlook.com>
2025-01-31 20:51:15 -0500

    Update Demo2.java

--- src/team/Demo2.java
+++ src/team/Demo2.java
@@ -3,6 +3,16 @@ package team;
 public class Demo2 {
 
   	public static void main(String[] args) {
+				/**
+			From Team 1
+			*/
+				/**
+			From Team 1
+			*/
+				/**
+			From Team 1
+			*/
+		
 		
 		int bat = 3;
 		int ball = 2;
